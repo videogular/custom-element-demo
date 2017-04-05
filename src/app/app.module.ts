deafa97bda2809f93f863e260a6da468495a5b5b
Raul <elecash@gmail.com>
2017-04-05 14:24:01 +0200

    Demo Start: Creating a simple player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VgCoreModule } from 'videogular2/core';
 import { VgControlsModule } from 'videogular2/controls';
-import { TimeViewerComponent } from './time-viewer/time-viewer.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        TimeViewerComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
